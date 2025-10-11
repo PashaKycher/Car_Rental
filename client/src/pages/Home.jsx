d1df14635f063b488070e4c3830a398fb316e2bc
PashaKycher <Pasha.Kycher89@gmail.com>
2025-10-11 07:50:17 +0300

    fix car loading

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
+import { useAppContext } from '../context/AppContext'
 import NewsLetter from '../components/NewsLetter'
 
 const Home = ({ setShowLogin }) => {
+  const { fetchCars } = useAppContext()
+
+  useEffect(() => {
+    fetchCars()
+  }, []);
   return (
     <>
       {/* first section */}
